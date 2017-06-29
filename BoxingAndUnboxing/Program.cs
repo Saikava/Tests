e3105996f4ba56c5f05c740ea254658b494c9e51
i.fialtov <i.filatov@devinotele.com>
2017-06-29 14:51:41 +0300

    initial

---
+++ BoxingAndUnboxing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BoxingAndUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tester = new StringsTester(10_000_000);
+            
+            Mesure(tester.ToStringWithoutBoxing);
+            Mesure(tester.ToStringWithBoxing);
+
+            Mesure(tester.ToStringArrayWithoutBoxing);
+            Mesure(tester.ToStringArrayWithBoxing);
+
+            Console.ReadKey();
+        }
+
+        static void Mesure(Func<string> getResult)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine($"Executing {getResult.Method.Name}...");
+            Console.WriteLine($"Result: {getResult()}");
+            sw.Stop();
+            Console.WriteLine($"Execution time: {sw.Elapsed}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ BoxingAndUnboxing/StringsTester.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BoxingAndUnboxing
+{
+    public class StringsTester
+    {
+        private readonly int _n;
+        private const string DefaultResult = "DONE";
+
+        public StringsTester(int n)
+        {
+            _n = n;
+        }
+
+        public string ToStringWithBoxing() => TestString(ToStringWithBoxing);
+
+        public string ToStringWithoutBoxing() => TestString(ToString);
+
+        public string ToStringArrayWithoutBoxing() => TestStringArray(ToString);
+
+        public string ToStringArrayWithBoxing() => TestStringArray(ToStringWithBoxing);
+
+        private string TestStringArray(Func<int, string> toStringFunc)
+        {
+            var strings = new string[_n];
+            for (var i = 0; i < _n; i++)
+            {
+                strings[i] = toStringFunc(i);
+            }
+            
+            return DefaultResult;
+        }
+
+        private string TestString(Func<int, string> toStringFunc)
+        {
+            var str = string.Empty; 
+            for (var i = 0; i < _n; i++)
+            {
+                str = (str + toStringFunc(i)).GetHashCode().ToString();
+                Console.WriteLine(str);
+            }
+            
+            return str;
+        }
+
+        private string ToStringWithBoxing(int x) => ((Object) x).ToString();
+        
+        private string ToString(int x) => x.ToString();
+    }
+}
